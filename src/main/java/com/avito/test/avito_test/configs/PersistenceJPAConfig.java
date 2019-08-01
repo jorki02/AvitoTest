37dd4491a2a1829d5f2832bd3ff8f489160e8ef1
Alex <jorki02@gmail.com>
2019-08-01 13:09:54 +0300

    AvitoTask properties

--- src/main/java/com/avito/test/avito_test/configs/PersistenceJPAConfig.java
+++ src/main/java/com/avito/test/avito_test/configs/PersistenceJPAConfig.java
@@ -1,7 +1,11 @@
 package com.avito.test.avito_test.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -17,7 +21,12 @@ import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
+@PropertySource("classpath:database.properties")
+@ConfigurationProperties
 public class PersistenceJPAConfig {
+
+    @Autowired
+    private Environment env;
 
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
@@ -36,10 +45,10 @@ public class PersistenceJPAConfig {
     @Bean
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
-        dataSource.setUsername( "postgres" );
-        dataSource.setPassword( "root" );
+        dataSource.setDriverClassName(env.getProperty("driver-class-name"));
+        dataSource.setUrl(env.getProperty("url"));
+        dataSource.setUsername( env.getProperty("user") );
+        dataSource.setPassword( env.getProperty("password") );
         return dataSource;
     }
 
@@ -59,7 +68,7 @@ public class PersistenceJPAConfig {
     Properties additionalProperties() {
         Properties properties = new Properties();
         properties.setProperty("hibernate.hbm2ddl.auto", "update");
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL95Dialect");
+        properties.setProperty("hibernate.dialect", env.getProperty("dialect"));
 
         return properties;
     }

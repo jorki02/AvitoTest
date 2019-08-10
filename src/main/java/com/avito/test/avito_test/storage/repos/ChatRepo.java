95ff27f0297ab27632170dfcf5ee469b2a4afa03
alex <jorki02@gmail.com>
2019-08-10 15:50:41 +0300

    Fixed chats order

--- src/main/java/com/avito/test/avito_test/storage/repos/ChatRepo.java
+++ src/main/java/com/avito/test/avito_test/storage/repos/ChatRepo.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public interface ChatRepo extends CrudRepository<Chat, Integer> {
 
-    @Query("FROM Chat c JOIN c.users cu LEFT JOIN c.messages msg WHERE cu.id = ?1 ORDER BY msg.createdAt")
+    @Query("FROM Chat c JOIN c.users cu LEFT JOIN c.messages msg WHERE cu.id = ?1 ORDER BY msg.createdAt DESC")
     Collection<Chat> findAllChatsByUserIdSorted(Integer userId);
 
 }
